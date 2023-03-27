830b902f245810ce6a52e54bc6ce961bf43141ba
danielfordfc <daniel.ford@fundingcircle.com>
2023-03-27 21:26:23 +0100

    nested arrays of structs in structs are nulling out

--- kafka/python-data-generator/data_generator.py
+++ kafka/python-data-generator/data_generator.py
@@ -77,34 +77,57 @@ def generate_fake_data(schema):
                     fake_data[field_name] = ''.join(random.choices(['a', 'b', 'c', 'd', 'e', 'f'], k=10))
                 elif field_type.type == 'boolean':
                     fake_data[field_name] = random.choices(['True', 'False'])
+            elif isinstance(field_type, TimestampMillisSchema):
+                fake_data[field_name] = int(time.time() * 1000)
             elif isinstance(field_type, EnumSchema):
                 fake_data[field_name] = random.choice(field_type.symbols)
             elif isinstance(field_type, UnionSchema):
-                # Handle nullable union of primitives
-                primitive_count = 0
-                primitive_list = []
-                for n in field_type.schemas:
-                    if isinstance(n, PrimitiveSchema):
-                        primitive_count +=1
-                        if n.type != 'null':
-                            primitive_list.append(n.type)
-                if primitive_count == len(field_type.schemas):
-                    fake_data[field_name] = ''.join(random.choices(['a', 'b', 'c', 'd', 'e', 'f'], k=10))
-                    continue
+
                 # Handle union fields
                 non_null_types = [t for t in field_type.schemas if t.type != 'null']
                 rnd_idx = random.randint(0, len(non_null_types)-1)
-                fake_data[field_name] = generate_fake_data(non_null_types[rnd_idx])
+
+                # Handle nullable union of primitives
+                # if primitive_count == len(field_type.schemas):
+                #     # this is returning strings always, needs to handle all types of primitives.
+                #     fake_data[field_name] = process_primitive_type(field_type)
+                #     continue
+                if not isinstance(non_null_types[rnd_idx], RecordSchema) and not isinstance(non_null_types[rnd_idx], UnionSchema):
+                    fake_data[field_name] = process_primitive_type(non_null_types[rnd_idx])
+                else:
+                    fake_data[field_name] = generate_fake_data(non_null_types[rnd_idx])
             else:
                 raise ValueError
     return fake_data
 
 
-# if __name__ == "__main__":
-#     path = os.path.realpath(os.path.dirname(__file__))
-#     schema_parse = avro.schema.parse(open(f"{path}/schemas/inputs/user_generic.avsc", "rb").read())
+def process_primitive_type(field_type):
+    # Handle primitive fields
+    if field_type.type == 'int':
+        # if 'logicalType' in field_type.other_props and field_type.other_props['logicalType'] == 'date':
+        #     fake_data[field_name] = datetime.date(random.randint(1970, 2022), random.randint(1, 12), random.randint(1, 28)).strftime("%Y-%m-%d")
+        # else:
+        return random.randint(0, 100)
+    elif field_type.type == 'long':
+        if 'logicalType' in field_type.other_props and field_type.other_props['logicalType'] == 'timestamp-millis':
+            return int(time.time() * 1000)
+        else:
+            return random.randint(0, 1000000)
+    elif field_type.type == 'float':
+        return random.uniform(0, 100)
+    elif field_type.type == 'double':
+        return random.uniform(0, 1000000)
+    elif field_type.type == 'string':
+        return ''.join(random.choices(['a', 'b', 'c', 'd', 'e', 'f'], k=10))
+    elif field_type.type == 'boolean':
+        return random.choices(['True', 'False'])
 
-#     data_payload = generate_fake_data(schema_parse)
 
-#     with open(f"{path}/schemas/outputs/user_generic_payload.json", "w") as f:
-#         f.write(json.dumps(data_payload, indent=4))
+if __name__ == "__main__":
+    path = os.path.realpath(os.path.dirname(__file__))
+    schema_parse = avro.schema.parse(open(f"{path}/schemas/inputs/fca-afmas.json", "rb").read())
+
+    data_payload = generate_fake_data(schema_parse)
+
+    with open(f"{path}/schemas/outputs/fca-afmas-2.json", "w") as f:
+        f.write(json.dumps(data_payload, indent=4))
--- kafka/python-data-generator/test_data_generator.py
+++ kafka/python-data-generator/test_data_generator.py
@@ -4,7 +4,7 @@ import tempfile
 import os
 
 from data_generator import generate_fake_data
-
+import json
 """
 I would like to write a test suite to test different schemas, whereby I'll take a schema,
 run the function on it to generate the payload, 
@@ -21,8 +21,9 @@ def test_generate_fake_data():
         # i.e. User field with a namespace of  "namespace": "confluent.io.examples.serialization.avro", "name": "User",
         # would need to resolve to 'name': 'confluent.io.examples.serialization.avro.User',
 
-        avro.schema.parse(open(f"{path}/schemas/inputs/user_generic.avsc", "rb").read()),
-        avro.schema.parse(open(f"{path}/schemas/inputs/user_specific.avsc", "rb").read())
+        #avro.schema.parse(open(f"{path}/schemas/inputs/user_generic.avsc", "rb").read()),
+        #avro.schema.parse(open(f"{path}/schemas/inputs/user_specific.avsc", "rb").read()),
+        avro.schema.parse(open(f"{path}/schemas/inputs/fca-afmas.json", "rb").read())
     ]
     for schema in schemas:
 
@@ -32,13 +33,20 @@ def test_generate_fake_data():
         # Serialize the fake data to Avro binary format
         with tempfile.NamedTemporaryFile(delete=False) as f:
             fastavro.writer(f, schema.to_json(), [fake_data])
+            # its basically whinging that there's no data with name 'name' when trying to coerce the json into the sch,
+            # because only 'confluent.io.examples.serialization.avro.name' exists...
             f.flush()
 
             # Read the serialized Avro message back in and check that its schema matches the original schema
             f.seek(0)
             reader = fastavro.reader(f)
-            assert reader.writer_schema == schema.to_json()
+
+            # when namespaces arent handled in the fake_data_generator, then schema.to_json() contains namespace field,
+            # that isn't present in the writer_schema. to_canonical_json seems to work though
+            assert reader.writer_schema == json.loads(json.dumps(schema.to_canonical_json()))
+            #assert reader.writer_schema == schema.to_json()
 
             # Check that the data in the serialized Avro message matches the generated fake data
-            deserialized_data = next(reader)  # Get the first (and only) element from the reader object
+            deserialized_data = next(reader)
+
             assert deserialized_data == fake_data
